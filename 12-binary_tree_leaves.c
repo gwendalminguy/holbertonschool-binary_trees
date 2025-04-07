ffe0e50075719c19efa7c1414dcb76c8a3fb5128
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-07 17:08:51 +0200

    Added comments for clarity

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,8 +11,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	/* Returning 1 if the node is a leaf */
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
+	/* Returning the sum of the number of leaves of both children otherwise */
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }

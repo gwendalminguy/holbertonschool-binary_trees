2db1f395650f5171297466bf1996d26edd619602
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-07 16:14:06 +0200

    Changed function binary_tree_depth documentation

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - ...
- * @tree:
+ * binary_tree_depth - measures the depth of a binary tree
+ * @tree: pointer to the root node
  *
- * Return: ...
+ * Return: depth of the tree
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {

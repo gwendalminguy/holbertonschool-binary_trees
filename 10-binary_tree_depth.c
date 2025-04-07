9d6cf9f2b679bd7148142c40bb8ab79ad625f44c
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-07 15:38:59 +0200

    Created function measuring the depth of a binary tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - ...
+ * @tree:
+ *
+ * Return: ...
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree->parent == NULL)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}

ffaf4ced7daa73754675d46fc0d2be1aa9823b00
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-08 13:04:48 +0200

    Corrected with betty coding style

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_sibling - finds the sibling of a node in a binary tree
- * @tree: pointer to the root node
+ * @node: pointer to the node
  *
- * Return: pointer to the node
+ * Return: pointer to the sibling node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {

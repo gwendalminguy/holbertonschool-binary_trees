42909b1daf91809fd930a5c4ac7198946d542c32
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-08 21:43:38 +0200

    Created function going through a binary tree using level order traversal

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,67 @@
+#include "binary_trees.h"
+
+void binary_tree_children(binary_tree_t **list, void (*func)(int));
+
+/**
+ * binary_tree_levelorder - goes through a binary tree using level order traversal
+ * @tree: pointer to the root node
+ * @func: pointer to function
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	binary_tree_t *currentList[2];
+
+	if (tree != NULL && func != NULL)
+	{
+		/* Adding the root node to the list */
+		currentList[0] = (binary_tree_t *)tree;
+
+		/* Terminating the list with NULL */
+		currentList[1] = NULL;
+
+		/* Processing the list */
+		binary_tree_children(currentList, func);
+	}
+}
+
+/**
+ * binary_tree_children - processes a list of nodes and creates a new one with children
+ * @list: list of nodes
+ * @func: pointer to function
+ */
+void binary_tree_children(binary_tree_t **currentList, void (*func)(int))
+{
+	binary_tree_t *childrenList[100];
+	int i = 0, j = 0;
+
+	if (currentList[0] != NULL)
+	{
+		while (currentList[i] != NULL)
+		{
+			/* Processing the node */
+			func((currentList[i])->n);
+			
+			/* Adding its left child to the new list */
+			if ((currentList[i])->left != NULL)
+			{
+				childrenList[j] = (currentList[i])->left;
+				j++;
+			}
+
+			/* Adding its right child to the new list */
+			if ((currentList[i])->right != NULL)
+			{
+				childrenList[j] = (currentList[i])->right;
+				j++;
+			}
+
+			i++;
+		}
+
+		/* Terminating the new list with NULL */
+		childrenList[j] = NULL;
+
+		/* Processing the new list */
+		binary_tree_children(childrenList, *func);
+	}
+}

29e60ee4aeb64dacdd70e21f43558f9beda44c46
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-03 19:00:39 +0200

    Created function checking if a binary tree is an AVL tree

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+#include "14-binary_tree_balance.c"
+#include "110-binary_tree_is_bst.c"
+
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is an AVL tree
+ * @tree: pointer to the root node
+ *
+ * Return: 1 if tree is an AVL tree ; 0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int factor = 0;
+
+	factor = binary_tree_balance(tree);
+
+	if (factor < -1 || factor > 1)
+		return (0);
+
+	if (!binary_tree_is_bst(tree))
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else if (tree->left == NULL)
+		return (binary_tree_is_avl(tree->right));
+	else if (tree->right == NULL)
+		return (binary_tree_is_avl(tree->left));
+	else
+		return (binary_tree_is_avl(tree->left) * binary_tree_is_avl(tree->right));
+
+	return (1);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -54,6 +54,7 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);

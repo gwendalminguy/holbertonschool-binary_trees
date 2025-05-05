754390d0408cb4bee8d909c048d87e2096ef3118
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-06 01:41:27 +0200

    Corrected order of procedur

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
 
+int total_nodes(const binary_tree_t *tree);
+heap_t *find_last(heap_t **current_list, int nodes);
+heap_t *move_last(heap_t *last, heap_t *root);
 heap_t *swap_down(heap_t *parent, heap_t *child);
 
 /**
@@ -11,48 +14,140 @@ heap_t *swap_down(heap_t *parent, heap_t *child);
 int heap_extract(heap_t **root)
 {
 	heap_t *current = *root;
-	int value = 0;
+	heap_t *current_list[2];
+	heap_t *last, *temp;
+	int nodes = 0, value = 0, first = 1;
 
 	if (current != NULL)
 	{
+		nodes = total_nodes(*root);
 		value = current->n;
 
-		/* Swapping down the node until it has no child */
-		while (current->left != NULL || current->right != NULL)
+		/*Finding last node in levelorder */
+		current_list[0] = current;
+		current_list[1] = NULL;
+		if (current->left != NULL || current->right != NULL)
 		{
-			if (current->right == NULL)
-				current = swap_down(current, current->left);
-			else if (current->left == NULL)
-				current = swap_down(current, current->right);
+			last = find_last(current_list, nodes);
+			last = move_last(last, current);
+		}
+		else
+		{
+			free(current);
+			*root = NULL;
+			return (value);
+		}
+
+		/* Swapping down the node until it has no child */
+		while (last->left != NULL || last->right != NULL)
+		{
+			if (last->right == NULL)
+				last = swap_down(last, last->left);
+			else if (last->left == NULL)
+				last = swap_down(last, last->right);
 			else
 			{
-				if (current->left->n > current->right->n)
-					current = swap_down(current, current->left);
+				if (last->left->n > last->right->n)
+					last = swap_down(last, last->left);
 				else
-					current = swap_down(current, current->right);
+					last = swap_down(last, last->right);
 			}
 
-			if (*root == current)
-				*root = current->parent;
+			if (first)
+				*root = last->parent;
+
+			first = 0;
 		}
-
-		/* Removing node from tree */
-		if (current->parent != NULL)
-		{
-			if (current == current->parent->left)
-				current->parent->left = NULL;
-			else
-				current->parent->right = NULL;
-		}
-
-		/* Case of the last node */
-		else
-			*root = NULL;
-
-		/* Freeing the node */
-		free(current);
 	}
 	return (value);
+}
+
+/**
+ * total_nodes - counts total number of nodes in a binary tree
+ * @tree: pointer to the root node
+ *
+ * Return: total number of nodes
+ */
+int total_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	/* Returning 1 if the node is a leaf */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	/* Returning the sum of the number of nodes of both children otherwise */
+	return (total_nodes(tree->left) + total_nodes(tree->right) + 1);
+}
+
+/**
+ * find_last - finds the last node in levelorder
+ * @current_list: list of nodes
+ * @nodes: total number of nodes
+ *
+ * Return: pointer to the last node
+ */
+heap_t *find_last(heap_t **current_list, int nodes)
+{
+	heap_t *children_list[1024];
+	int i = 0, j = 0, end = 0;
+
+	while (current_list[i] != NULL)
+	{
+		children_list[j] = current_list[i]->left;
+		j++;
+
+		children_list[j] = current_list[i]->right;
+		j++;
+
+		if (nodes == 1)
+		{
+			end = 1;
+			break;
+		}
+
+		i++;
+		nodes--;
+	}
+
+	/* Returning node if end is reached */
+	if (end)
+		return (current_list[i]);
+
+	/* Terminating the new list with NULL */
+	children_list[j] = NULL;
+
+	/* Processing the new list */
+	return (find_last(children_list, nodes));
+}
+
+/**
+ * move_last - replaces root node with last node
+ * @last: pointer to the last node
+ * @root: pointer to the root node
+ *
+ * Return: pointer to the new root node
+ */
+heap_t *move_last(heap_t *last, heap_t *root)
+{
+	if (last->parent->right == last)
+		last->parent->right = NULL;
+	else
+		last->parent->left = NULL;
+
+	last->parent = NULL;
+	last->left = root->left;
+	last->right = root->right;
+
+	if (last->left != NULL)
+		last->left->parent = last;
+	if (last->right != NULL)
+		last->right->parent = last;
+
+	free(root);
+
+	return (last);
 }
 
 /**
